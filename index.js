4dd200a4a5156b48f66a34eb876e23d1c2358bc6
Guilherme Reis <guilhermejosereis@outlook.com>
2022-01-26 18:06:34 -0400

    Fix

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get(
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
       );
 
-      await page.goto("https://you.com");
+      await page.goto("https://you.com/");
 
       const html = await page.evaluate(() => {
         return {
